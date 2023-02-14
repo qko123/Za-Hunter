206c5d6b4345c9f8a138a4362634df9b2062e018
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-14 09:22:07 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Quinn Kozak on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
