b3c8396af7a46fff99f889d0d9107f4397e91a99
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-14 10:07:23 -0600

    Almost finished but have to fix pizza markers

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -27,16 +27,17 @@ struct ContentView: View {
             showsUserLocation: true,
             userTrackingMode: $userTrackingMode,
             annotationItems: places) { place in
-            MapMarker(coordinate: place.annotation.coordinate)
-        }
+                MapMarker(coordinate: place.annotation.coordinate)
+            }
             .onAppear(perform: {
                 performSearch(item: "Pizza")
             })
     }
+    
     func performSearch(item: String) {
         let searchRequest = MKLocalSearch.Request()
-            searchRequest.naturalLanguageQuery = item
-            searchRequest.region = region
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
         let search = MKLocalSearch(request: searchRequest)
         
         search.start { response, error in
@@ -46,13 +47,12 @@ struct ContentView: View {
                     annotation.coordinate = mapItem.placemark.coordinate
                     annotation.title = mapItem.name
                     places.append(Place(annotation: annotation, mapItem: mapItem))
-
+                    
                 }
             }
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -62,4 +62,15 @@ struct Place: Identifiable {
     let id = UUID()
     let annotation: MKPointAnnotation
     let mapItem: MKMapItem
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
+    }
 }
